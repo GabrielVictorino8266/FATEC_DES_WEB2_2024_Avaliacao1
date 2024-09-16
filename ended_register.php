d30dcf03d2f81e4c03f7a715e175aab0565a771f
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2024-09-16 20:24:05 -0300

    Created save file feature on ended-register.php

--- ended_register.php
+++ ended_register.php
@@ -8,17 +8,22 @@ if(!isset($_SESSION['user_id']) || $_SESSION['user_id'] != "coordenacao"){
 
 if(isset($_POST['register-request'])){
     if(isset($_POST['laboratory']) && $_POST['date'] && $_POST['requesting'] && $_POST['course']){
-        echo "Register finished!";
-        //saving datacontent
-        
+        //saving data content
+        if($_POST['course'] == 'DSM'){
+            $file = "DSM.txt";
+            $content = "\n" . $_POST['laboratory'] . " | " . $_POST['date'] . " | " . $_POST['requesting'];
+            file_put_contents($file, $content, FILE_APPEND);
+        }
+        if($_POST['course'] == 'GE'){
+            $file = "GE.txt";
+            $content = "\n" . $_POST['laboratory'] . " | " . $_POST['date'] . " | " . $_POST['requesting'];
+            file_put_contents($file, $content, FILE_APPEND);        }
     }
 }else{
     echo "Register not finished!";
     header('location: register.php');
     exit;
 }
-
-
 ?>
 
 
